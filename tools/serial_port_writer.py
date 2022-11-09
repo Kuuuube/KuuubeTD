c6587ba64d1339ba332eca139f2f46835cd2feeb
kuuube <hexagonisalie@gmail.com>
2022-11-09 16:34:47 -0500

    Feat: Add serial port writer tool

---
+++ tools/serial_port_writer.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+SERIAL_PORT = "COM3"
+SERIAL_RATE = 19200
+BYTE_SIZE = 8
+STOPBITS = 1
+
+def send_command(command, command_name):
+    serial_port.write((command + "\r").encode())
+    output = serial_port.readline()
+    if (output != b""):
+        print(command_name + ": ", end="")
+        print(output.decode('utf-8'))
+    time.sleep(0.2)
+
+serial_port = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_RATE, bytesize=BYTE_SIZE, stopbits=STOPBITS, timeout=1)
+
+while (True):
+    print("Enter command: ")
+    command = input()
+    send_command(command, "Output")
