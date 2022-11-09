411ff303af7b0186af5c62bd845487a34e4d1811
kuuube <hexagonisalie@gmail.com>
2022-11-09 14:22:42 -0500

    Fix/Feat: Fix pressure being off by 1 in some cases, clean up parser, and parse all buttons

--- KT-0405-R/KT_0405_R_parser.py
+++ KT-0405-R/KT_0405_R_parser.py
@@ -3,19 +3,19 @@ def parse(report):
 
     pos_y = (((int(report[3]) & 0x03) << 14) + (int(report[4]) << 7) + int(report[5]))
 
-    pressure = (((int(report[6]) & 0x7f) * 2) + ((int(report[3]) & 0x04) >> 2))
+    pressure = (((int(report[6]) & 0x3f) << 1) + ((int(report[3]) & 0x04) >> 2))
 
-    if (int(report[6]) & 0x40):
-        pressure = (~(pressure - 1) & 0x7f) * -1
-
-    pressure = pressure + 127
-
-    button_pressed = bool(int(report[0]) & 0x08)
+    if (int(report[6]) & 0x40 == 0):
+        pressure = pressure + int(0x80)
 
     proximity = bool(int(report[0]) & 0x40)
 
-    button = int(report[3]) >> 3 if button_pressed else 0
+    button_flag = bool(int(report[0]) & 0x08)
 
-    pen_tip_press = bool(button & 0x01)
+    pen_tip = bool(int(report[3]) & 0x08)
 
-    return (pos_x, pos_y, pressure, pen_tip_press, proximity)
+    top_side_button_or_eraser = bool(int(report[3]) & 0x20)
+
+    bottom_side_button = bool(int(report[3]) & 0x10)
+
+    return (pos_x, pos_y, pressure, pen_tip, proximity)
