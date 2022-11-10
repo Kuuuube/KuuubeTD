9e89e18cfd8414a580fd6e8e841165f4a823fbee
kuuube <hexagonisalie@gmail.com>
2022-11-10 12:08:17 -0500

    Feat: Make wacom_serial_tablet_diagnostics.py more user friendly

--- tools/wacom_serial_tablet_diagnostics.py
+++ tools/wacom_serial_tablet_diagnostics.py
@@ -1,7 +1,22 @@
 import serial
 import time
+import serial.tools.list_ports
+import re
 
-SERIAL_PORT = "COM3"
+ports = serial.tools.list_ports.comports()
+
+print("Available ports:")
+
+for port in sorted(ports):
+    try:
+        print(re.sub(" .*", "", str(port)))
+    except Exception:
+        print(port)
+print("")
+
+print("Enter serial port path for tablet:")
+
+SERIAL_PORT = input()
 SERIAL_RATE = 9600
 BYTE_SIZE = 8
 STOPBITS = 1
@@ -17,6 +32,8 @@ def send_command(command, command_name):
 serial_port = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_RATE, bytesize=BYTE_SIZE, stopbits=STOPBITS, timeout=1)
 
 baud_rates = [9600, 19200, 4800, 2400]
+
+print("Attempting to request tablet information. Do not put your pen on the tablet.")
 
 for baud_rate in baud_rates:
     serial_port.baudrate = baud_rate
