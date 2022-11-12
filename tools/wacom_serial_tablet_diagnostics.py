ec4e6e65ad753ecc2f36cd2dc24da8686a82936a
kuuube <hexagonisalie@gmail.com>
2022-11-11 19:11:35 -0500

    Feat: Add test command to diagnostics tool and specify lines to read

--- tools/wacom_serial_tablet_diagnostics.py
+++ tools/wacom_serial_tablet_diagnostics.py
@@ -21,12 +21,15 @@ SERIAL_RATE = 9600
 BYTE_SIZE = 8
 STOPBITS = 1
 
-def send_command(command, command_name):
+def send_command(command, command_name, lines_to_read):
     serial_port.write((command + "\r").encode())
-    output = serial_port.readline()
-    if (output != b""):
-        print(command_name + ": ", end="")
-        print(output.decode('utf-8'))
+    lines_read = 0
+    while (lines_read < lines_to_read):
+        output = serial_port.readline()
+        if (output != b""):
+            print(command_name + ": ", end="")
+            print(output.decode('utf-8'))
+        lines_read += 1
     time.sleep(0.2)
 
 serial_port = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_RATE, bytesize=BYTE_SIZE, stopbits=STOPBITS, timeout=1)
@@ -38,9 +41,10 @@ print("Attempting to request tablet information. Do not put your pen on the tabl
 for baud_rate in baud_rates:
     serial_port.baudrate = baud_rate
     print("Testing " + str(baud_rate) + " baud rate")
-    send_command("~#", "Tablet ID")
-    send_command("~R", "Read Setting")
-    send_command("~C", "Tablet Size")
+    send_command("~#", "Tablet ID", 1)
+    send_command("~R", "Read Setting", 1)
+    send_command("TE", "Test", 2)
+    send_command("~C", "Tablet Size", 1)
 
 print("Finished")
 input()
