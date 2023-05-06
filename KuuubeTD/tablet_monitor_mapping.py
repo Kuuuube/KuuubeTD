448f8effbc22926640db170a9f659e10156ccf88
kuuuube <hexagonisalie@gmail.com>
2023-05-06 14:21:58 -0400

    Fix: Clamp input outside set tablet size

--- KuuubeTD/tablet_monitor_mapping.py
+++ KuuubeTD/tablet_monitor_mapping.py
@@ -2,9 +2,17 @@ from internal_constants import *
 from user_constants import *
 
 def map_x(pos_x):
+    if pos_x > TABLET_MAX_X_POS + TABLET_OFFSET_X:
+        pos_x = TABLET_MAX_X_POS + TABLET_OFFSET_X
+    if pos_x < TABLET_OFFSET_X:
+        pos_x = TABLET_OFFSET_X
     return int(clamp((VMULTI_MONITOR_SCALING_X * (pos_x - TABLET_OFFSET_X) + VMULTI_MONITOR_OFFSET_X), VMULTI_MONITOR_OFFSET_X, VMULTI_MONITOR_OFFSET_X + VMULTI_MONITOR_RES_X))
 
 def map_y(pos_y):
+    if pos_y > TABLET_MAX_Y_POS + TABLET_OFFSET_Y:
+        pos_y = TABLET_MAX_Y_POS + TABLET_OFFSET_Y
+    if pos_y < TABLET_OFFSET_Y:
+        pos_y = TABLET_OFFSET_Y
     return int(clamp((VMULTI_MONITOR_SCALING_Y * (pos_y - TABLET_OFFSET_Y) + VMULTI_MONITOR_OFFSET_Y), VMULTI_MONITOR_OFFSET_Y, VMULTI_MONITOR_OFFSET_Y + VMULTI_MONITOR_RES_Y))
 
 def clamp(input, minimum, maximum):
